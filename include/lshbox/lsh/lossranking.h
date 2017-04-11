333d43a7dde36f029489753e96a63e7f81816917
Jinfeng Li <lijinf8@gmail.com>
2017-04-11 12:50:19 +0800

    Add lossranking

--- include/lshbox/lsh/lossranking.h
+++ include/lshbox/lsh/lossranking.h
@@ -5,6 +5,7 @@ class LossRanking{
 public:
     typedef typename ACCESSOR::Value value;
     typedef typename ACCESSOR::DATATYPE DATATYPE;
+    typedef unsigned long long BIDTYPE;
 
     template<typename LSHTYPE>
     LossRanking(
@@ -17,7 +18,7 @@ public:
         std::vector<float> queryFloats = mylsh.getHashFloats(0, domin);
         
         // ranking by std::sort, low efficiency
-        for ( std::map<unsigned, std::vector<unsigned> >::iterator it = mylsh.tables[0].begin(); it != mylsh.tables[0].end(); ++it) {
+        for ( std::map<BIDTYPE, std::vector<unsigned> >::iterator it = mylsh.tables[0].begin(); it != mylsh.tables[0].end(); ++it) {
 
             std::vector<bool> bucketBits = mylsh.unsignedToBools(it->first);
             float dst = 0;
@@ -26,18 +27,18 @@ public:
                     dst += fabs(queryFloats[bIdx]); 
                 }
             }
-            dstToBks_.push_back(std::pair<float, unsigned>(dst, it->first));
+            dstToBks_.push_back(std::pair<float, BIDTYPE>(dst, it->first));
         }
         assert(dstToBks.size() == mylsh.tables[0].size());
 
         std::sort(dstToBks_.begin(), 
             dstToBks_.end(), 
-            [] (const std::pair<float, unsigned>& a, const std::pair<float, unsigned>& b ) {
+            [] (const std::pair<float, BIDTYPE>& a, const std::pair<float, BIDTYPE>& b ) {
                 return a.first < b.first;
             });
     }
 
-    unsigned getNextBID(){
+    BIDTYPE getNextBID(){
         return dstToBks_[index++].second;
         // while (proCol_ == dstToBks_[proRow_].size()) {
         //     proCol_ = 0;
@@ -46,23 +47,37 @@ public:
         // return dstToBks_[proRow_][proCol_++];
     }
 
+    bool nextBucketExisted() {
+        if (index == dstToBks_.size()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
     void operator()(unsigned key){
-        scanner_(key);
+        numItemsProbed_++;
+        // scanner_(key);
     }
 
-    void reportCDD(){
-        // report probed items
-        lshbox::Scanner<ACCESSOR> thisScan = scanner_;
-        thisScan.topk().genTopk();
-        std::vector<std::pair<float, unsigned>> topk 
-            = thisScan.topk().getTopk();
+    int getNumItemsProbed() { // get number of items probed;
+        return numItemsProbed_;
     }
+
+    // void reportCDD(){
+    //     // report probed items
+    //     lshbox::Scanner<ACCESSOR> thisScan = scanner_;
+    //     thisScan.topk().genTopk();
+    //     std::vector<std::pair<float, unsigned>> topk 
+    //         = thisScan.topk().getTopk();
+    // }
 
     lshbox::Scanner<ACCESSOR> getScanner(){
         return scanner_;
     }
 private:
-    std::vector<std::pair<float, unsigned> > dstToBks_;
+    std::vector<std::pair<float, BIDTYPE> > dstToBks_;
     unsigned long long index = 0;
+    unsigned long long numBucketsProbed_ = 0;
+    int numItemsProbed_ = 0;
     lshbox::Scanner<ACCESSOR> scanner_;
 };
