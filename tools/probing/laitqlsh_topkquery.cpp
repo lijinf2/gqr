1179850342e9d39e577986cec6dd710da9399cbd
Jinfeng Li <lijinf8@gmail.com>
2017-04-26 12:33:10 +0800

    add parallel training

--- tools/probing/laitqlsh_topkquery.cpp
+++ tools/probing/laitqlsh_topkquery.cpp
@@ -64,13 +64,14 @@ int main(int argc, char const *argv[])
     else
     {
         lshbox::laItqLsh<DATATYPE>::Parameter param;
-        param.L = 10;  // number of tables
+        param.L = 5;  // number of tables
         param.D = data.getDim();
         param.N = 16;  // number of bits
-        param.S = 6000; //must be the size of data, which will be used to init tables,  number of vectors in the training set
+        param.S = 60000; //must be the size of data, which will be used to init tables,  number of vectors in the training set
         param.I = 50;
         mylsh.reset(param);
-        mylsh.train(data);
+        // mylsh.train(data);
+        mylsh.trainAll(data);
         mylsh.hash(data);
         mylsh.save(file);
         std::cout << "CONSTRUCTING TIME: " << timer.elapsed() << "s." << std::endl;
