13a2486a1e986ff389112b5c5aba3161260a10db
Jinfeng Li <lijinf8@gmail.com>
2017-10-19 15:54:28 +0800

    remove run

--- run/train-sift.sh
+++
@@ -1,6 +0,0 @@
-ls -al >> /dev/null
-ls -al bin/ >> /dev/null
-# # train multiple tables
-
-# # one table
-tmp/train-sift datasets/sift10m/sift10m-centered.bin datasets/sift10m/1-sift10m-20bit-50iter.itq datasets/sift10m/1000q-top1000.ben 0 1000000000 1
