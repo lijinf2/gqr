7ee1fde736bbd24f65687a529b8e56ccf6a6e0ac
Jinfeng Li <lijinf8@gmail.com>
2017-10-19 15:58:45 +0800

    remove test

--- test/fv_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <lshbox/query/fv.h>
-#include <string>
-#include <iostream>
-int main() {
-    std::cout << "hello world" << std::endl;
-    FV fv(4);
-    std::string str = fv.toString();
-    std::cout << str << std::endl;
-}
