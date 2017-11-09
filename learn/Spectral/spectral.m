974609a2d3da14f136538a7116b1077401483674
daixinyan <381675152@qq.com>
2017-11-09 16:48:43 +0800

    PreTest:add module, including compressing and learning for spectral hashing.

---
+++ learn/Spectral/spectral.m
@@ -0,0 +1,73 @@
+addpath('../../MatlabFunc/Tools')
+addpath('../../MatlabFunc/ANNS/Hashing/Unsupervised')
+
+dataset = 'audio';
+codelength = 12;            
+nHashTable = 1; % multiple hash tables do not help accuracy, but only slow down anns
+
+
+method = 'spectral'
+baseCodeFile = ['./hashingCodeTXT/',method,'table',upper(dataset),num2str(codelength),'b_',num2str(nHashTable),'tb.txt'];              
+queryCodeFile = ['./hashingCodeTXT/',method,'query',upper(dataset),num2str(codelength),'b_',num2str(nHashTable),'tb.txt'];
+modelFile = ['./hashingCodeTXT/',method,'model',upper(dataset),num2str(codelength),'b_',num2str(nHashTable),'tb.txt'];
+        
+% zero-centered dataset
+trainset = double(fvecs_read (['../../data/',dataset,'/',dataset,'_base.fvecs']));
+testset = fvecs_read (['../../data/',dataset,'/',dataset,'_query.fvecs']);
+trainset = trainset';
+meanTrainset = mean(trainset);
+trainset = trainset - repmat(meanTrainset, size(trainset, 1), 1);
+testset = testset';
+testset = testset - repmat(meanTrainset, size(testset, 1), 1);
+
+if codelength > 128
+    disp(['codelenth ',num2str(codelength),' not supported yet!']);
+    return;
+end
+
+disp('==============================');
+disp([method,' ',num2str(codelength),'bit ',dataset,' nTable=',num2str(nHashTable)]);
+disp('==============================');
+
+
+
+[cardinality, dimension] = size(trainset)
+numQueries = size(testset, 1)
+
+modelFid = fopen(modelFile,'wt');
+% #of tables, dimension, codelength, #data points, #num queries
+fprintf(modelFid,'%d %d %d %d %d\n' , nHashTable, dimension, codelength, cardinality, numQueries);
+fprintf(modelFid, '%f ', meanTrainset);
+fprintf(modelFid, '\n');
+baseCodeFid = fopen(baseCodeFile,'wt');
+queryCodeFid = fopen(queryCodeFile,'wt');
+for j =1:nHashTable                
+    % train and test model
+    trainStr = ['[model, trainB ,train_elapse] = ',method,'_learn(trainset, codelength);'];
+    testStr = ['[testB,test_elapse] = ',method,'_compress(testset, model);'];
+    eval(trainStr);
+    eval(testStr);
+   
+    % save model
+    for i = 1 : size(model.pc, 1);
+        fprintf(modelFid,'%f ',model.pc(i,:));
+        fprintf(modelFid,'\n');
+    end
+
+    % save base codes 
+    for i = 1 : size(trainB,1);
+        fprintf(baseCodeFid,'%g ',trainB(i,:));
+        fprintf(baseCodeFid,'\n');
+    end
+    
+    % save query codes
+    for i = 1 : size(testB,1);
+        fprintf(queryCodeFid,'%g ',testB(i,:));
+        fprintf(queryCodeFid,'\n');
+    end
+end
+fclose(modelFid)
+fclose(baseCodeFid);
+fclose(queryCodeFid);
+disp('==============================');
+
---
+++ learn/Spectral/spectral_learn.m
@@ -0,0 +1,56 @@
+function [model, B,elapse] = PCAH_learn(A, maxbits)
+%
+% Input
+%   X = features matrix [Nsamples, Nfeatures]
+%   maxbits = number of bits (nbits do not need to be a multiple of 8)
+%
+%
+% Spectral Hashing
+% Y. Weiss, A. Torralba, R. Fergus. 
+% Advances in Neural Information Processing Systems, 2008.
+
+tmp_T = tic;
+
+[Nsamples Ndim] = size(X);
+nbits = maxbits;
+
+% algo:
+% 1) PCA
+npca = min(nbits, Ndim);
+[pc, l] = eigs(cov(X), npca);
+X = X * pc; % no need to remove the mean
+
+
+% 2) fit uniform distribution
+mn = prctile(X, 5);  mn = min(X)-eps;
+mx = prctile(X, 95);  mx = max(X)+eps;
+
+
+% 3) enumerate eigenfunctions
+R=(mx-mn);
+maxMode=ceil((nbits+1)*R/max(R));
+
+nModes=sum(maxMode)-length(maxMode)+1;
+modes = ones([nModes npca]);
+m = 1;
+for i=1:npca
+    modes(m+1:m+maxMode(i)-1,i) = 2:maxMode(i);
+    m = m+maxMode(i)-1;
+end
+modes = modes - 1;
+omega0 = pi./R;
+omegas = modes.*repmat(omega0, [nModes 1]);
+eigVal = -sum(omegas.^2,2);
+[yy,ii]= sort(-eigVal);
+modes=modes(ii(2:nbits+1),:);
+
+
+% 4) store paramaters
+model.pc = pc;
+model.mn = mn;
+model.mx = mx;
+model.mx = mx;
+model.modes = modes;
+
+elapse = toc(tmp_T);
+end
