a6b95b99bdfb63becd029c9be4d695c368559da2
daixinyan <381675152@qq.com>
2018-01-02 13:26:33 +0800

    Revert to simhash version.

--- learn/SIP/precess.m
+++ learn/SIP/precess.m
@@ -6,8 +6,8 @@ function [ max_norm ] = precess(train, test)
 norm_row = sum(train.^2, 2);
 max_norm = 31*max(norm_row);
 
-train(:, end+1) = max_norm;
-train(:, end+1) = 0;
+% train(:, end+1) = max_norm - norm_row;
+% train(:, end+1) = 0;
 
-test(:, end+1) = 0;
-test(:, end+1) = max_norm;
+% test(:, end+1) = 0;
+% test(:, end+1) = max_norm - sum(test.^2, 2);
