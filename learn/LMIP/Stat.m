7ce0e3962c75e7fecc0b331931b06ee01ee0dfcd
xinyandai <381675152@qq.com>
2018-02-02 15:20:20 +0800

    Add module: normalized LMIP.

--- learn/LMIP/Stat.m
+++ learn/LMIP/Stat.m
@@ -1,7 +1,7 @@
 addpath('../../MatlabFunc/Tools')
 addpath('../../MatlabFunc/ANNS/Hashing/Unsupervised')
 
-dataset = 'audio';
+dataset = 'movielens';
 
 % zero-centered dataset
 trainset = double(fvecs_read (['../../data/',dataset,'/',dataset,'_base.fvecs']));
@@ -10,8 +10,15 @@ trainset = trainset';
 testset = fvecs_read (['../../data/',dataset,'/',dataset,'_query.fvecs']);
 testset = testset' ;
 
-norms = sum(trainset.^2, 2);
+norms = sum(trainset.^2, 2).^(0.5);
 testNorms = sum(testset.^2, 2);
-histogram(norms);
-bits = 16;
+% histogram(norms);
+bits = 63;
 prc = prctile(norms, linspace(0, 100, bits+1));
+% stem(prc)
+
+norm_median = median(norms);
+eps = 0.0001;
+while length(find(norms<norm_median-eps)) + length(find(norms>norm_median+eps)) > 0.3*length(norms)
+    eps = eps + 0.0001
+end
