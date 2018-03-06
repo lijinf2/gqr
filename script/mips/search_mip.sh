03afb47349ffed7966d9e36e3c7026cef857c1be
xinyandai <381675152@qq.com>
2018-03-06 15:02:40 +0800

    Move mips script to a single folder.

--- script/mips/search_mip.sh
+++ script/mips/search_mip.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-mkdir ../build 
-cd ../build 
+gqr_dir="../../"
+mkdir ${gqr_dir}build
+cd ${gqr_dir}build
 # cmake ../ -DCMAKE_BUILD_TYPE=Debug
 cmake ../ -DCMAKE_BUILD_TYPE=Release
-make search 2>&1 | tee ../script/log.txt
-cd ../script
+make search 2>&1 | tee ../script/mips/log.txt
+cd ../script/mips
+
 log=`grep error log.txt`
 if [ "$log" != "" ]; then
     exit
@@ -13,6 +15,17 @@ fi
 ### search methods
 ## mips
 
+# # nlmip
+hash_method="NLMIP"
+# query_method="NR"
+# query_method="IMIP"
+query_method="PRE_SORT"
+codelength="16b_64i" # 12b 27b 58b
+num_tables="1tb"
+metric="product"
+# dataset="movielens"
+dataset="netflix"
+
 # lmip
 # hash_method="LMIP"
 # query_method="LM"
@@ -20,16 +33,6 @@ fi
 # num_tables="1tb"
 # metric="product"
 
-# # nlmip
-# hash_method="NLMIP"
-# query_method="NR"
-# # query_method="IMIP"
-# codelength="25b_128i" # 12b 27b 58b
-# num_tables="1tb"
-# metric="product"
-# #
-# # # dataset="movielens"
-# dataset="netflix"
 
 # simpleLSH
 # hash_method="SIM"
@@ -38,16 +41,16 @@ fi
 # num_tables="1tb"
 # metric="angular"
 # dataset="m2a_movielens"
-# # dataset="m2a_netflix"
+# dataset="m2a_netflix"
 
 # # # ALSH
 # hash_method="IntRankALSH"
-hash_method="ALSHRank"
-codelength="32b" # 16b 32b 64b
-num_tables="1tb"
-metric="product"
+# hash_method="ALSHRank"
+# codelength="32b" # 16b 32b 64b
+# num_tables="1tb"
+# metric="product"
 # dataset="movielens"
-dataset="netflix"
+# dataset="netflix"
 
 # # E2LSH
 # hash_method="E2LSH"
@@ -60,18 +63,19 @@ dataset="netflix"
 
 base_format="fvecs"
 
-model_file="../learn/${hash_method}/hashingCodeTXT/${hash_method}model${dataset^^}${codelength}_${num_tables}.txt"
-base_file="../data/${dataset}/${dataset}_base.fvecs"
-base_bits_file="../learn/${hash_method}/hashingCodeTXT/${hash_method}table${dataset^^}${codelength}_${num_tables}.txt"
-query_file="../data/${dataset}/${dataset}_query.fvecs"
+model_file="${gqr_dir}learn/${hash_method}/hashingCodeTXT/${hash_method}model${dataset^^}${codelength}_${num_tables}.txt"
+base_file="${gqr_dir}data/${dataset}/${dataset}_base.fvecs"
+base_bits_file="${gqr_dir}learn/${hash_method}/hashingCodeTXT/${hash_method}table${dataset^^}${codelength}_${num_tables}.txt"
+query_file="${gqr_dir}data/${dataset}/${dataset}_query.fvecs"
 
-benchmark_file="../data/${dataset}/${dataset}_groundtruth.lshbox"
+benchmark_file="${gqr_dir}data/${dataset}/${dataset}_groundtruth.lshbox"
 if [ $metric != "euclidean" ]
 then
-    benchmark_file="../data/${dataset}/${dataset}_${metric}_groundtruth.lshbox"
+    benchmark_file="${gqr_dir}data/${dataset}/${dataset}_${metric}_groundtruth.lshbox"
 fi
 
-../build/bin/search\
+
+../../build/bin/search\
     --hash_method=$hash_method \
     --query_method=$query_method \
     --base_format=$base_format \
