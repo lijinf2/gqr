12913f22186394ef9d6a47e6f001ab373023dff0
Jinfeng Li <lijinf8@gmail.com>
2018-03-07 17:13:50 +0800

    improve glove_to_fvecs

--- script/data_to_fvecs/glove/glove_to_fvecs.sh
+++ script/data_to_fvecs/glove/glove_to_fvecs.sh
@@ -1,5 +1,6 @@
 mkdir output
 
+dataset="sample.glove.twitter.27B.200d.txt"
 g++ -O3 glove_to_fvecs.cpp -o ./output/glove_to_fvecs.bin
 
-./output/glove_to_fvecs.bin sample.glove.twitter.27B.200d.txt ./output/sample.glove1.2m_base.fvecs
+./output/glove_to_fvecs.bin $dataset ./output/$dataset.fvecs
